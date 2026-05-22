c028315694e48b28616c19c681f2ad4879508c34
sumaiazaman <saktar50.cse@gmail.com>
2026-05-22 15:13:08 +0600

    Fix CI workflow and macro compatibility with Laravel 11

--- src/StringableExtrasServiceProvider.php
+++ src/StringableExtrasServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Sumaiazaman\StringableExtras;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 use Illuminate\Support\Stringable;
 
 class StringableExtrasServiceProvider extends ServiceProvider
@@ -10,11 +11,11 @@ class StringableExtrasServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Stringable::macro('whenDoesntContain', function ($needles, callable $callback, ?callable $default = null) {
-            return $this->when($this->doesntContain($needles), $callback, $default);
+            return $this->when(Str::doesntContain((string) $this, $needles), $callback, $default);
         });
 
         Stringable::macro('whenDoesntContainAll', function (array $needles, callable $callback, ?callable $default = null) {
-            return $this->when(! $this->containsAll($needles), $callback, $default);
+            return $this->when(! Str::containsAll((string) $this, $needles), $callback, $default);
         });
     }
 }
